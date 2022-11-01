3f032575559f4958f5442773723e9a4ea6737427
kapsona777 <lasha.kapanadze.1@btu.edu.ge>
2022-11-01 17:04:50 +0400

    update

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -22,14 +22,18 @@ const Home = () => {
         <div className="container home-page">
             <div className="text-zone">
                 <h1>  
+                <div id="upper"> 
+                <AnimatedLetters letterClass={letterClass}
+                strArray={nameArray}
+                idx={15}/>
                 <img src={LogoTitle} alt="developer" />
-                    <AnimatedLetters letterClass={letterClass}
-                    strArray={nameArray}
-                    idx={15}/>
-                    <br/> 
-                    <AnimatedLetters letterClass={letterClass}
-                    strArray={jobArray}
-                    idx={17}/>
+                </div> 
+                <div id="lower">
+                <br/> 
+                <AnimatedLetters letterClass={letterClass}
+                strArray={jobArray}
+                idx={17}/>
+                </div>
                 </h1> 
                 <Link to="/contact" className='flat-button'>CONTACT ME</Link>
             </div>
